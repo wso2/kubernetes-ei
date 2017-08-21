3935b021d2a682728afede4555d70af84249575c
dilanua <dilana@wso2.com>
2017-08-21 21:35:36 +0530

    Add pattern 2

---
+++ patterns/pattern-2/images/build-images.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# ------------------------------------------------------------------------
+#
+# Copyright 2017 WSO2, Inc. (http://wso2.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+#
+# ------------------------------------------------------------------------
+
+# Product Context
+product=wso2ei
+version=6.1.1
+patternNumber=2
+
+pushd ./mysql/ > /dev/null 2>&1
+/bin/bash build-profile-image.sh
+popd > /dev/null 2>&1
+
+pushd ./analytics/ > /dev/null 2>&1
+/bin/bash build-profile-image.sh
+popd > /dev/null 2>&1
+
+pushd ./integrator/ > /dev/null 2>&1
+/bin/bash build-profile-image.sh
+popd > /dev/null 2>&1
