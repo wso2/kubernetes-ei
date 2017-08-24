db9af1630fff8f094e972d89cdcf6a601b587793
dilanua <dilana@wso2.com>
2017-08-24 22:11:11 +0530

    Update README and Restructure artifacts

--- pattern-1/deploy-kubernetes.sh
+++ pattern-1/deploy-kubernetes.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+artifacts=./artifacts
+
 # rdbms
-echo '[1] deploying external databases ...'
-kubectl create -f rdbms-service.yaml
-kubectl create -f rdbms-deployment.yaml
+echo '[1] Deploying external databases ...'
+kubectl create -f ${artifacts}/rdbms-service.yaml
+kubectl create -f ${artifacts}/rdbms-deployment.yaml
 sleep 60s
 
 # integrator
-echo '[2] deploying integrator ...'
-kubectl create -f integrator-service.yaml
-kubectl create -f integrator-deployment.yaml
+echo '[2] Deploying integrator ...'
+kubectl create -f ${artifacts}/integrator-service.yaml
+kubectl create -f ${artifacts}/integrator-deployment.yaml
 sleep 60s
 
-echo 'finished ...'
+echo 'Finished ...'
 echo 'To access the console, try https://<node-ip>:<node-port>/carbon in your favorite browser ...'
--- pattern-1/undeploy-kubernetes.sh
+++ pattern-1/undeploy-kubernetes.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # integrator
-echo '[1] un-deploying integrator ...'
+echo '[1] Un-deploying integrator ...'
 kubectl delete deployment wso2ei-pattern1-integrator-deployment
 kubectl delete service wso2ei-pattern1-integrator-service
 sleep 60s
 
 # rdbms
-echo '[2] un-deploying external databases ...'
+echo '[2] Un-deploying external databases ...'
 kubectl delete deployment wso2ei-pattern1-mysql-deployment
 kubectl delete service wso2ei-pattern1-mysql-service
 sleep 60s
 
-echo 'finished ...'
+echo 'Finished ...'
