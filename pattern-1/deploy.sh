158eeaffa4ceda5b84a55e444b2a235b211b1af3
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2017-08-25 14:34:39 +0530

    Update pattern-1/deploy.sh

--- pattern-1/deploy.sh
+++ pattern-1/deploy.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-# params
-artifacts=./artifacts
-
-# rdbms
-echo '[1] Deploying external databases ...'
-kubectl create -f ${artifacts}/rdbms-service.yaml
-kubectl create -f ${artifacts}/rdbms-deployment.yaml
+# mysql
+echo 'Deploying WSO2 Integrator Databases...'
+kubectl create -f rdbms-service.yaml
+kubectl create -f rdbms-deployment.yaml
 sleep 60s
 
 # integrator
-echo '[2] Deploying integrator ...'
-kubectl create -f ${artifacts}/integrator-service.yaml
-kubectl create -f ${artifacts}/integrator-deployment.yaml
+echo 'Deploying WSO2 Integrator...'
+kubectl create -f integrator-service.yaml
+kubectl create -f integrator-deployment.yaml
 sleep 60s
 
 echo 'Finished ...'
-echo 'To access the console, try https://<node-ip>:<node-port>/carbon in your favorite browser ...'
+echo 'To access the console, try https://<node-ip>:<node-port>/carbon in your browser.
