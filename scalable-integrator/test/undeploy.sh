3739c5d745cdd300ff5f49c86b3142c6388b8bd3
chirangaalwis <chirangaalwis@gmail.com>
2018-05-16 22:02:09 +0530

    Create separate Ingresses for Integrator gateway

--- scalable-integrator/test/undeploy.sh
+++ scalable-integrator/test/undeploy.sh
@@ -24,6 +24,7 @@ function echoBold () {
 # removing the ingress resource
 echoBold 'Un-deploying Ingress...'
 kubectl delete -f ../ingresses/integrator-gateway-ingress.yaml
+kubectl delete -f ../ingresses/integrator-gateway-ssl-ingress.yaml
 kubectl delete -f ../ingresses/integrator-ingress.yaml
 
 # integrator
