6c1b5cd7e2a88a1c9612cbb3f0fe917963f50216
ThilinaManamgoda <thilinama@wso2.com>
2019-05-13 15:51:10 +0530

    Finalize deployment

--- integrator-analytics/scripts/deploy.sh
+++ integrator-analytics/scripts/deploy.sh
@@ -26,47 +26,6 @@ function echoBold () {
     ${ECHO} -e $'\e[1m'"${1}"$'\e[0m'
 }
 
-function usage () {
-    echoBold "This script automates the installation of WSO2 EI Integrator Analytics Kubernetes resources\n"
-    echoBold "Allowed arguments:\n"
-    echoBold "-h | --help"
-    echoBold "--wu | --wso2-username\t\tYour WSO2 username"
-    echoBold "--wp | --wso2-password\t\tYour WSO2 password"
-    echoBold "--cap | --cluster-admin-password\tKubernetes cluster admin password\n\n"
-}
-
-WSO2_SUBSCRIPTION_USERNAME=''
-WSO2_SUBSCRIPTION_PASSWORD=''
-ADMIN_PASSWORD=''
-
-# capture named arguments
-while [ "$1" != "" ]; do
-    PARAM=`echo $1 | awk -F= '{print $1}'`
-    VALUE=`echo $1 | awk -F= '{print $2}'`
-
-    case ${PARAM} in
-        -h | --help)
-            usage
-            exit 1
-            ;;
-        --wu | --wso2-username)
-            WSO2_SUBSCRIPTION_USERNAME=${VALUE}
-            ;;
-        --wp | --wso2-password)
-            WSO2_SUBSCRIPTION_PASSWORD=${VALUE}
-            ;;
-        --cap | --cluster-admin-password)
-            ADMIN_PASSWORD=${VALUE}
-            ;;
-        *)
-            echoBold "ERROR: unknown parameter \"${PARAM}\""
-            usage
-            exit 1
-            ;;
-    esac
-    shift
-done
-
 # create a new Kubernetes Namespace
 ${KUBECTL} create namespace wso2
 
@@ -76,11 +35,8 @@ ${KUBECTL} create serviceaccount wso2svc-account -n wso2
 # switch the context to new 'wso2' namespace
 ${KUBECTL} config set-context $(${KUBECTL} config current-context) --namespace=wso2
 
-# create a Kubernetes Secret for passing WSO2 Private Docker Registry credentials
-${KUBECTL} create secret docker-registry wso2creds --docker-server=docker.wso2.com --docker-username=${WSO2_SUBSCRIPTION_USERNAME} --docker-password=${WSO2_SUBSCRIPTION_PASSWORD} --docker-email=${WSO2_SUBSCRIPTION_USERNAME}
-
 # create Kubernetes Role and Role Binding necessary for the Kubernetes API requests made from Kubernetes membership scheme
-${KUBECTL} create --username=admin --password=${ADMIN_PASSWORD} -f ../../rbac/rbac.yaml
+${KUBECTL} create -f ../../rbac/rbac.yaml
 
 # create Kubernetes ConfigMaps
 echoBold 'Creating ConfigMaps...'
@@ -105,7 +61,7 @@ ${KUBECTL} create -f ../dashboard/integrator-server-dashboard-service.yaml
 # MySQL
 echoBold 'Deploying WSO2 Enterprise Integrator and Enterprise Integrator Analytics Databases using MySQL...'
 ${KUBECTL} create -f ../extras/rdbms/mysql/mysql-deployment.yaml
-sleep 10s
+sleep 15s
 
 # persistent storage
 echoBold 'Creating persistent volume and volume claim...'
@@ -113,15 +69,15 @@ ${KUBECTL} create -f ../integrator/integrator-volume-claims.yaml
 ${KUBECTL} create -f ../extras/rdbms/mysql/mysql-persistent-volume-claim.yaml
 ${KUBECTL} create -f ../volumes/persistent-volumes.yaml
 ${KUBECTL} create -f ../extras/rdbms/volumes/persistent-volumes.yaml
-sleep 10s
+sleep 15s
 
 # Integrator
 echoBold 'Deploying WSO2 Integrator and Analytics...'
 ${KUBECTL} create -f ../analytics/integrator-analytics-deployment.yaml
-sleep 4m
+sleep 5m
 
 ${KUBECTL} create -f ../dashboard/integrator-server-dashboard-deployment.yaml
-sleep 30s
+sleep 35s
 
 ${KUBECTL} create -f ../integrator/integrator-deployment.yaml
 sleep 30s
